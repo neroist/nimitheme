4ab4d172255ad6b8b7f28d253a6caa566a60b904
t-h-rk-turner <71654487+t-h-rk-turner@users.noreply.github.com>
2023-03-26 17:03:06 -0400

    update docs & readme

--- docs/index.nim
+++ docs/index.nim
@@ -24,13 +24,17 @@ nbText: """
 Nimitheme is a Nimble package that provides additional themes for [nimib](https://github.com/pietroppeter/nimib).
 Currently supports the following themes:
   - [a11yana](https://a11yana.com/)
+  - ads (couldn't find a link)
+  - [almond](https://alvaromontoro.github.io/almond.css/)
   - [attri](https://raj457036.github.io/attriCSS/)
   - [awsm](https://igoradamenko.github.io/awsm.css/)
   - [axist](https://ruanmartinelli.github.io/axist/)
   - [bahunya](https://hakanalpay.com/bahunya/)
   - [bare](https://barecss.com/)
   - [basic](https://vladocar.github.io/Basic.css/)
+  - [bonsai](https://www.bonsaicss.com/)
   - [downstyler](https://waldyrious.net/downstyler/)
+  - [github-markdown](https://github.com/sindresorhus/github-markdown-css)
   - [holiday.css](https://holidaycss.js.org/)
   - [latex.css](https://davidrzs.github.io/latexcss/)
   - [markdown](https://markdowncss.github.io/)
@@ -46,6 +50,7 @@ Currently supports the following themes:
   - [tacit.css](https://yegor256.github.io/tacit/)
   - [style.css](https://ungoldman.com/style.css/)
   - [w3.css](https://www.w3schools.com/w3css/defaulT.asp)
+  - [W3c Core Styles](https://www.w3.org/StyleSheets/Core/Overview)
   - [writ.css](https://writ.cmcenroe.me/)
 
 In addition, support for [Dark Reader](https://github.com/darkreader/darkreader) and
