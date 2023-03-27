5828f261aef05a6a28a6f916950bc6641bbc626d
Grave <pmpiga1@gmail.com>
2023-03-26 21:03:35 -0400

    set header padding

--- src/nimitheme/simple.nim
+++ src/nimitheme/simple.nim
@@ -3,8 +3,16 @@ import nimib
 
 const
   simple* = """<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/kevquirk/simple.css/simple.min.css">"""
+  simpleStyle* = """
+<style>
+  header {
+    padding-top: 16px !important;
+  }
+</style>
+"""
 
 proc useSimple*(doc: var NbDoc) = 
   doc.useDefault()
   doc.darkMode()
   doc.context["stylesheet"] = simple
+  doc.context["nb_style"] = doc.context["nb_style"].vString & '\n' & simpleStyle
