d7f3325b878ee107640577235a1ac5f1b6ce80aa
t-h-rk-turner <71654487+t-h-rk-turner@users.noreply.github.com>
2023-03-31 23:39:41 -0400

    add nimitheme index

---
+++ docsrc/themes/index.nim
@@ -0,0 +1,93 @@
+import nimib
+
+nbInit
+
+nb.title = "Nimitheme Index"
+
+nbText: """
+## Nimitheme Index
+
+Here is an index of all the available themes in nimitheme:
+
+- Ads
+  - [Gazette](ads/gazette.html)
+  - [Medium](ads/medium.html)
+  - [Notebook](ads/notebook.html)
+  - [Tufte](ads/tufte.html)
+- Almond
+  - [Almond](almond/almond.html)
+  - [Lite](almond/lite.html)
+- Attri
+  - [Bright Light Green](attri/bright_light_green.html)
+  - [Dark Fairy Pink](attri/dark_fairy_pink.html)
+  - [Dark Forest Green](attri/dark_forest_green.html)
+  - [Light Fairy Pink](attri/light_fairy_pink.html)
+  - [Midnight Green](attri/midnight_green.html)
+- Awsm
+  - [Big Stone](awsm/big_stone.html)
+  - [Black](awsm/black.html)
+  - [Gondola](awsm/gondola.html)
+  - [Mischka](awsm/mischka.html)
+  - [Pastel Pink](awsm/pastel_pink.html)
+  - [Pearl Lusta](awsm/pearl_lusta.html)
+  - [Tasman](awsm/tasman.html)
+  - [White](awsm/white.html)
+- Bamboo
+  - [Dark](bamboo/dark.html)
+  - [White](bamboo/white.html)
+- Basic
+  - [Dark Classic](basic/dark_classic.html)
+  - [Dark Orange](basic/dark_orange.html)
+  - [Light Aquamarine](basic/light_aquamarine.html)
+  - [Light Classic](basic/light_classic.html)
+- Github Markdown
+  - [Dark](github_markdown/dark.html)
+  - [Light](github_markdown/light.html)
+- Markdown
+  - [Air](markdown/air.html)
+  - [Modest](markdown/modest.html)
+  - [Retro](markdown/retro.html)
+  - [Splendor](markdown/splendor.html)
+- Pico
+  - [Dark](pico/dark.html)
+  - [Light](pico/light.html)
+- Sakura
+  - [Dark solarized](sakura/dark_solarized.html)
+  - [Dark](sakura/dark.html)
+  - [Earthly](sakura/earthly.html)
+  - [Ink](sakura/ink.html)
+  - [Pink](sakura/pink.html)
+  - [Sakura](sakura/sakura.html)
+  - [Vader](sakura/vader.html)
+- W3c
+  - [Chocolate](w3c/chocolate.html)
+  - [Midnight](w3c/midnight.html)
+  - [Modernist](w3c/modernist.html)
+  - [Old Style](w3c/old_style.html)
+  - [Steely](w3c/steely.html)
+  - [Swiss](w3c/swiss.html)
+  - [Traditional](w3c/traditional.html)
+  - [Ultramarine](w3c/ultramarine.html)
+- [A11yana](a11yana.html)
+- [Axist](axist.html)
+- [Bahunya](bahunya.html)
+- [Bare](bare.html)
+- [Bonsai](bonsai.html)
+- [Downstyler](downstyler.html)
+- [Holiday](holiday.html)
+- [Latex](latex.html)
+- [Marx](marx.html)
+- [Mercury](mercury.html)
+- [MVP](mvp.html)
+- [New](new.html)
+- [Nimibujo](nimibujo.html)
+- [No Class](no_class.html)
+- [Simple](simple.html)
+- [Style](style.html)
+- [Tacit](tacit.html)
+- [W3](w3.html)
+- [Writ](writ.html)
+- [YoRHa](yorha.html)
+"""
+
+nbSave
