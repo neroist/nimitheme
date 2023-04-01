d38cf4836e62eb8783f92046a2785c0f34a3bec7
t-h-rk-turner <71654487+t-h-rk-turner@users.noreply.github.com>
2023-04-01 00:10:53 -0400

    add index doc for basic theme

---
+++ docsrc/themes/basic/index.nim
@@ -0,0 +1,18 @@
+import nimitheme
+import nimib
+
+nbInit(useBasic)
+
+nb.title = "Basic Theme Set"
+
+nbText: """
+## Basic Theme Set
+
+In this theme set, there are the following themes:
+  - [Dark Classic](dark_classic.html)
+  - [Dark Orange](dark_orange.html)
+  - [Light Aquamarine](light_aquamarine.html)
+  - [Light Classic](light_classic.html)
+"""
+
+nbSave
