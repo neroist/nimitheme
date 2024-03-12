a277ee54679e09360926dc84cd438000fa723203
neroist <pmpiga1@gmail.com>
2024-03-12 10:40:51 -0400

    add `style` param to `fixCode`

--- src/nimitheme/utils.nim
+++ src/nimitheme/utils.nim
@@ -24,18 +24,20 @@ template useStyle*(stylesheets: static[openArray[string]], customStyle: string =
   when customStyle.len > 0:
     doc.context["nb_style"] = doc.context["nb_style"].vString & '\n' & customStyle
 
-template fixCode*(nb: NbDoc) =
-  const customStyle = """
-  <style>
-    pre {
-      font-size: 1.2em;
-    }
-  </style>
-  """
+template fixCode*(nb: NbDoc, style: bool = false) =
+  when style:
+    const customStyle = """
+    <style>
+      pre {
+        font-size: 1.2em;
+      }
+    </style>
+    """
+    
+    doc.context["nb_style"] = doc.context["nb_style"].vString & '\n' & customStyle
 
   doc.partials["nbCodeSource"] = "<pre class=\"nohighlight hljs nim\">{{&codeHighlighted}}</pre>"
 
-  doc.context["nb_style"] = doc.context["nb_style"].vString & '\n' & customStyle
 
 export join
     
