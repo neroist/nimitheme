398e1142de8ece7ba5630d113d95df7903fea189
niko <pmpiga1@gmail.com>
2024-06-20 14:45:26 -0400

    refactor `importexportdocs`

--- docsrc/all_themes.nim
+++ docsrc/all_themes.nim
@@ -3,15 +3,16 @@ import std/macros
 import std/hashes
 import std/os
 
-macro importExportDocs(ignoreDirs: static openArray[string], ignoreFiles: static openArray[string]; nbDocVar: static string): untyped =
-  ## Imports and export all files in themes/**
+macro importExport(root: static string, ignoreDirs, ignoreFiles: static openArray[string], nbDocVar: static string): untyped =
+  ## Imports and export all files in `root`
   ## 
   ## Adds all nimib documents to exported `allDocs` array
   ## 
-  ## :ignoreDirs: Which directories to ignore, relative to `themes/`
-  ## :ignoreDirs: Which files to ignore, relative to `themes/`
+  ## :root: Set root directory for imports.
+  ## :ignoreDirs: Which directories to ignore, relative to `root`
+  ## :ignoreFiles: Which files to ignore, relative to `root`
   ## :nbDocVar:   The name of the exported NbDoc across files
-  
+
   result = newStmtList(
     # imports and exports
     newNimNode(nnkImportStmt),
@@ -21,52 +22,46 @@ macro importExportDocs(ignoreDirs: static openArray[string], ignoreFiles: static
     newVarStmt(postfix(ident"allDocs", "*"), newNimNode(nnkBracket))
   )
 
-  const
-    # its shorter for me to set this to a variable since i use inlay hints
-    currentSourceDir = currentSourcePath().parentDir
-    self = currentSourcePath().splitFile().name
+  const self = currentSourcePath().splitFile().name
 
-  for file in walkDirRec(currentSourceDir, relative=true):
-    var
-      file = file.replace('\\', '/')
+  for file in walkDirRec(root, relative=true):
+    let
+      file = file.replace(DirSep, '/') # normalize path separators between OSes
       (fileDir, fileName, fileExt) = file.splitFile()
-    
-    # lets not import ourself now
-    if fileName == self: continue
 
-    # must be all nim files
-    if fileExt != ".nim": continue
+    if
+      # lets not import ourself now
+      fileName == self or
+      # must be all nim files
+      fileExt != ".nim" or
+      # ignore files from `ignoreFiles`
+      file in ignoreFiles or
+      # ignore dirs from `ignoreDirs`
+      fileDir in ignoreDirs:
+        continue
 
-    # ignore files from `ignoreFiles`
-    if file in ignoreFiles: continue
-    echo file
+    # the files may have similar names (e.g dir1/me.nim, dir2/me.nim), in 
+    # such case we append a unique string to each import in an `as` clause
+    # (so we can refer to it later during exporting & `allDocs`)
+    let
+      path = ident fileDir / fileName
 
+      # uses hashes from full file path for unique identifiers
+      # `file` is prepended for debug purposes
+      alias = ident file & $hash(file)
 
-    case fileDir
-    of "": # current directory
-      result[0].add ident(fileName)
-      result[1].add ident(fileName)
+    result[0].add infix(path, "as", alias)
+    result[1].add alias
 
-      # access var section array
-      # nnkVarSection -> nnkIdentDefs -> nnkBracket (array)
-      result[2][0][^1].add newDotExpr(ident fileName, ident nbDocVar)
-    elif fileDir in ignoreDirs: discard
-    else:
-      let
-        path = ident fileDir & '/' & fileName
+    # access var section array
+    # nnkVarSection -> nnkIdentDefs -> nnkBracket (array)
+    result[2][0][^1].add newDotExpr(alias, ident nbDocVar)
 
-        # uses hashes for unique identifiers
-        # `fileName` is prepended for debug purposes
-        alias = ident fileName & $hash(file)
+const currentSourceDir = currentSourcePath().parentDir
 
-      result[0].add infix(path, "as", alias)
-      result[1].add alias
-
-      # ditto, same here
-      result[2][0][^1].add newDotExpr(alias, ident nbDocVar)
-
-importExportDocs(
+importExport(
+  root        = currentSourceDir,
   ignoreDirs  = [],
-  ignoreFiles = ["themes/gendoc.nim", "compile_all.nim"],
+  ignoreFiles = ["themes/gendoc.nim", "compile_all.nim", "common.nim"],
   nbDocVar    = "doc"
 )
