3fde4a2a6a65822e961b6249af670af9b141faba
niko <pmpiga1@gmail.com>
2024-06-20 00:58:28 -0400

    fix `new` theme header (hr makes it look odd)

--- src/nimitheme/themes/new.nim
+++ src/nimitheme/themes/new.nim
@@ -8,8 +8,17 @@ import ../utils
 
 const 
   newCss* = """<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css">"""
+  newHeader* = """
+<header>
+<div class="nb-box">
+  <span>{{> header_left }}</span>
+  <span>{{> header_center }}</span>
+  <span>{{> header_right }}</span>
+</div>
+</header>"""
 
 proc useNew*(doc: var NbDoc) = 
   useStyle newCss, useDarkMode = true
   doc.highlight = a11yDark
+  doc.partials["header"] = newHeader
   
